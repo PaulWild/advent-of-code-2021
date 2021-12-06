ebad8f7824c1efc099c73681b7e2fb8eb15072ff
Paul Wild <paulwild@gmail.com>
2021-12-06 20:34:34 +0000

    Day 06 - small refactor

--- AdventOfCode/Days/Day06.cs
+++ AdventOfCode/Days/Day06.cs
@@ -7,61 +7,53 @@ public class Day06 : ISolution
     
 
     public string PartOne(IEnumerable<string> input)
-    {
-        var lanternFish = input.First()
-            .Split(",")
-            .Select(x => Convert.ToInt32(x)).GroupBy(x => x)
-            .ToDictionary(x => x.Key, x => x.Count());
-        
-        for (var day  = 1; day<= 80; day++)
-        {
-            var newLanternFishCycle = new Dictionary<int, int>();
-            foreach (var (key, value) in lanternFish)
-            {
-                if (key == 0)
-                {
-                    newLanternFishCycle.Add(8,value);
-                    newLanternFishCycle.AddOrUpdate(6, value,x => x+value);
-                }
-                else
-                {
-                    newLanternFishCycle.AddOrUpdate(key-1, value,x => x+value);
-                }
-            }
+    { 
+        var lanternFish = ParseInput(input);
+        lanternFish = SimulateLifecylcesForDays(lanternFish, 80);
 
-            lanternFish = newLanternFishCycle;
-        }
+        return lanternFish.Values.Sum().ToString();
+    }
+    
+    public string PartTwo(IEnumerable<string> input)
+    {
+        var lanternFish = ParseInput(input);
+        lanternFish = SimulateLifecylcesForDays(lanternFish, 256);
 
         return lanternFish.Values.Sum().ToString();
     }
 
-    public string PartTwo(IEnumerable<string> input)
+    private static Dictionary<int, long> SimulateLifecylcesForDays(Dictionary<int, long> startingPopulations, int days)
+    {
+        var populationCounts = startingPopulations;
+        for (var day = 1; day <= days; day++)
+        {
+            var newLanternFishCycle = new Dictionary<int, long>();
+            foreach (var (key, value) in populationCounts)
+            {
+                if (key == 0)
+                {
+                    newLanternFishCycle.Add(8, value);
+                    newLanternFishCycle.AddOrUpdate(6, value, x => x + value);
+                }
+                else
+                {
+                    newLanternFishCycle.AddOrUpdate(key - 1, value, x => x + value);
+                }
+            }
+
+            populationCounts = newLanternFishCycle;
+        }
+
+        return populationCounts;
+    }
+
+    private static Dictionary<int, long> ParseInput(IEnumerable<string> input)
     {
         var lanternFish = input.First()
             .Split(",")
             .Select(x => Convert.ToInt32(x)).GroupBy(x => x)
             .ToDictionary(x => x.Key, x => Convert.ToInt64(x.Count()));
-        
-        for (var day  = 1; day<= 256; day++)
-        {
-            var newLanternFishCycle = new Dictionary<int, long>();
-            foreach (var (key, value) in lanternFish)
-            {
-                if (key == 0)
-                {
-                    newLanternFishCycle.Add(8,value);
-                    newLanternFishCycle.AddOrUpdate(6, value,x => x+value);
-                }
-                else
-                {
-                    newLanternFishCycle.AddOrUpdate(key-1, value,x => x+value);
-                }
-            }
-
-            lanternFish = newLanternFishCycle;
-        }
-
-        return lanternFish.Values.Sum().ToString();
+        return lanternFish;
     }
 
     public int Day => 06;
