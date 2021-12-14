92f6f09175eedf5026dd7e836550769c2165b7e0
Paul Wild <paulwild@gmail.com>
2021-12-14 21:46:28 +0000

    Slight tidy

--- AdventOfCode/Days/Day14.cs
+++ AdventOfCode/Days/Day14.cs
@@ -1,74 +1,56 @@
-using AdventOfCode.Common;
-
+#pragma warning disable CS8601
 namespace AdventOfCode.Days;
 
 public class Day14 : ISolution
 {
-    public record polymer(char Left, char Right);
+    public record Polymer(char Left, char Right);
     
     public class ExpansionTree
     {
-        public ExpansionTree(polymer node)
+        public ExpansionTree(Polymer node)
         {
             Node = node;
         }
 
-        public polymer Node { get; set; }
+        public Polymer Node { get; }
         
-        public ExpansionTree Left { get; set; }
-        
-        public ExpansionTree Right { get; set; }
+        public ExpansionTree Left { get; set; } = null!;
 
-        public void ExpandedPolymer(int currentDepth, int depth, List<char> expansion)
-        {
-            if (currentDepth == 0)
-            {
-                expansion.Add(this.Node.Left);
-            }
-            if (depth == currentDepth)
-            {
-                expansion.Add(this.Node.Right);
-            }
-            else
-            {
-                this.Left.ExpandedPolymer(currentDepth + 1, depth, expansion);
-                this.Right.ExpandedPolymer(currentDepth + 1, depth, expansion);
-            }
-        }
+        public ExpansionTree Right { get; set; } = null!;
 
-        public long CountLetter(int currentDepth, int depth, char letter, Dictionary<(polymer, int), long> seenValues)
+        public long CountLetter(int currentDepth, int depth, char letter, Dictionary<(Polymer, int), long> seenValues)
         {
             var toReturn = 0L;
             if (currentDepth == 0)
             {
-                if (this.Node.Left == letter) toReturn++;
+                if (Node.Left == letter) toReturn++;
             }
 
             if (depth == currentDepth)
             {
-                if (this.Node.Left == letter) toReturn++;
+                if (Node.Left == letter) toReturn++;
                 return toReturn;
             }
 
-            if (seenValues.ContainsKey((this.Left.Node, currentDepth + 1)))
+            if (seenValues.ContainsKey((Left.Node, currentDepth + 1)))
             {
-                toReturn += seenValues[(this.Left.Node, currentDepth + 1)];
+                toReturn += seenValues[(Left.Node, currentDepth + 1)];
             }
             else
             {
-                var hmm = this.Left.CountLetter(currentDepth + 1, depth, letter, seenValues);
-                seenValues[(this.Left.Node, currentDepth + 1)] = hmm;
+                var hmm = Left.CountLetter(currentDepth + 1, depth, letter, seenValues);
+                seenValues[(Left.Node, currentDepth + 1)] = hmm;
                 toReturn += hmm;
             }
 
-            if (seenValues.ContainsKey((this.Right.Node, currentDepth + 1)))
+            if (seenValues.ContainsKey((Right.Node, currentDepth + 1)))
             {
-                toReturn += seenValues[(this.Right.Node, currentDepth + 1)];
+                toReturn += seenValues[(Right.Node, currentDepth + 1)];
             }
             else
             {
-                var hmm = this.Right.CountLetter(currentDepth + 1, depth, letter, seenValues);
-                seenValues[(this.Right.Node, currentDepth + 1)] = hmm;
+                var hmm = Right.CountLetter(currentDepth + 1, depth, letter, seenValues);
+                seenValues[(Right.Node, currentDepth + 1)] = hmm;
                 toReturn += hmm;
             }
 
@@ -77,7 +59,7 @@ public class Day14 : ISolution
         }
     }
 
-    public void ExpandTree(ExpansionTree tree, Dictionary<polymer,ExpansionTree> expandedTrees, Dictionary<polymer, (polymer, polymer)> polymerMap)
+    private static void ExpandTree(ExpansionTree tree, IDictionary<Polymer, ExpansionTree?> expandedTrees, IReadOnlyDictionary<Polymer, (Polymer, Polymer)> polymerMap)
     {
         var polymerLeft = polymerMap[tree.Node].Item1;
         var polymerRight = polymerMap[tree.Node].Item2;
@@ -109,107 +91,38 @@ public class Day14 : ISolution
     
     public string PartOne(IEnumerable<string> input)
     {
-        var polymerTemplate = input.First().ToCharArray().ToList() ;
-        
-        var pairInsertionRules = input.Skip(2)
-            .Select(x => x.Split("->", StringSplitOptions.TrimEntries))
-            .Select(pairs =>
-            {
-                var from = pairs[0].ToCharArray();
-                var polymer = new polymer(from[0], from[1]);
-                var leftExpansion = new polymer(from[0], pairs[1].ToCharArray().First());
-                var rightExpansion = new polymer(pairs[1].ToCharArray().First(), from[1]);
-                return KeyValuePair.Create(polymer, (leftExpansion, rightExpansion));
-            }).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-
-        var expansionTrees = new Dictionary<polymer,ExpansionTree>();
-        var allLetters = new HashSet<char>(pairInsertionRules.Keys.SelectMany(x => new[] { x.Left, x.Right }));
-        
-        var trees = new List<ExpansionTree>();
-        for (int i = 0; i < polymerTemplate.Count - 1; i++)
-        {
-            var tree = new ExpansionTree(new polymer(polymerTemplate[i], polymerTemplate[i + 1]));
-            ExpandTree(tree, expansionTrees, pairInsertionRules);
-            trees.Add(tree);
-        }
-
-
-        var counts = new List<long>();
-        foreach (var letter in allLetters)
-        {
-            var dp = new Dictionary<(polymer, int), long>();
-            var count = trees.Select(x => x.CountLetter(0, 10, letter,dp)).Sum();
-            if (trees.Last().Node.Right == letter) count++;
-            
-            if (count > 0) counts.Add(count);
-        }
-
-        return (counts.Max() - counts.Min()).ToString();
+        return SolveForNTimes(input.ToList(), 10);
     }
-
-    private static string PartOneSolution(IEnumerable<string> input)
-    {
-        var polymerTemplate = input.First().ToCharArray().ToList();
-        var pairInsertionRules = input.Skip(2)
-            .Select(x => x.Split("->", StringSplitOptions.TrimEntries))
-            .Select(pairs =>
-            {
-                var from = pairs[0].ToCharArray();
-                return KeyValuePair.Create((first: @from[0], second: @from[1]), pairs[1].ToCharArray()[0]);
-            }).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-
-        var polymer = polymerTemplate;
-
-        for (var step = 1; step <= 10; step++)
-        {
-            var newPolymer = new List<char>();
-            for (var i = 0; i < polymer.Count; i++)
-            {
-                if (i == (polymer.Count - 1))
-                {
-                    newPolymer.Add(polymer[i]);
-                }
-                else
-                {
-                    var rule = pairInsertionRules[(polymer[i], polymer[i + 1])];
-                    newPolymer.Add(polymer[i]);
-                    newPolymer.Add(rule);
-                }
-            }
-
-            polymer = newPolymer;
-            var countsFoo = polymer.GroupBy(x => x).Select(x => (x.Key, x.Count())).ToList();
-            //Console.WriteLine($"H: {countsFoo.First(x => x.Key == 'H').Item2}, Total: {countsFoo.Select(x => x.Item2).Sum()}, Percentage: {((double)countsFoo.First(x => x.Key == 'H').Item2)/((double)countsFoo.Select(x => x.Item2).Sum())}");
-            Console.WriteLine(string.Join("", polymer));
-        }
-
-        var counts = polymer.GroupBy(x => x).Select(x => x.Count()).ToList();
-        return (counts.Max() - counts.Min()).ToString();
-    }
-
-    // I think this might be some sort of DP problem. Keep track of all the changes from earlier in the chain but that doesn't seem right
+    
     public string PartTwo(IEnumerable<string> input)
     {
-        var polymerTemplate = input.First().ToCharArray().ToList() ;
-        
+        return SolveForNTimes(input.ToList(), 40);   
+    }
+
+
+    private static string SolveForNTimes(ICollection<string> input, int n)
+    {
+       
+        var polymerTemplate = input.First().ToCharArray().ToList();
+
         var pairInsertionRules = input.Skip(2)
             .Select(x => x.Split("->", StringSplitOptions.TrimEntries))
             .Select(pairs =>
             {
                 var from = pairs[0].ToCharArray();
-                var polymer = new polymer(from[0], from[1]);
-                var leftExpansion = new polymer(from[0], pairs[1].ToCharArray().First());
-                var rightExpansion = new polymer(pairs[1].ToCharArray().First(), from[1]);
+                var polymer = new Polymer(@from[0], @from[1]);
+                var leftExpansion = new Polymer(@from[0], pairs[1].ToCharArray().First());
+                var rightExpansion = new Polymer(pairs[1].ToCharArray().First(), @from[1]);
                 return KeyValuePair.Create(polymer, (leftExpansion, rightExpansion));
             }).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 
-        var expansionTrees = new Dictionary<polymer,ExpansionTree>();
+        var expansionTrees = new Dictionary<Polymer, ExpansionTree?>();
         var allLetters = new HashSet<char>(pairInsertionRules.Keys.SelectMany(x => new[] { x.Left, x.Right }));
-        
+
         var trees = new List<ExpansionTree>();
-        for (int i = 0; i < polymerTemplate.Count - 1; i++)
+        for (var i = 0; i < polymerTemplate.Count - 1; i++)
         {
-            var tree = new ExpansionTree(new polymer(polymerTemplate[i], polymerTemplate[i + 1]));
+            var tree = new ExpansionTree(new Polymer(polymerTemplate[i], polymerTemplate[i + 1]));
             ExpandTree(tree, expansionTrees, pairInsertionRules);
             trees.Add(tree);
         }
@@ -218,15 +131,15 @@ public class Day14 : ISolution
         var counts = new List<long>();
         foreach (var letter in allLetters)
         {
-            var dp = new Dictionary<(polymer, int), long>();
-            var count = trees.Select(x => x.CountLetter(0, 40, letter,dp)).Sum();
+            var dp = new Dictionary<(Polymer, int), long>();
+            var count = trees.Select(x => x.CountLetter(0, n, letter, dp)).Sum();
             if (trees.Last().Node.Right == letter) count++;
-            
+
             if (count > 0) counts.Add(count);
         }
 
         return (counts.Max() - counts.Min()).ToString();
     }
-
+    
     public int Day => 14;
 }
