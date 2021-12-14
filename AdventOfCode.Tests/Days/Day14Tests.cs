23e9aa79e03b85e57860b777a9ac36e1f76fc0be
Paul Wild <paulwild@gmail.com>
2021-12-14 21:23:50 +0000

    Day 14 - I CANNOT believe I got that

---
+++ AdventOfCode.Tests/Days/Day14Tests.cs
@@ -0,0 +1,67 @@
+using AdventOfCode.Days;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Tests.Days;
+
+
+public class Day14Tests
+{
+    private readonly ISolution _sut = new Day14();
+    
+    private readonly string[] _testData =
+    {
+        "NNCB",
+        "",
+        "CH -> B",
+        "HH -> N",
+        "CB -> H",
+        "NH -> C",
+        "HB -> C",
+        "HC -> B",
+        "HN -> C",
+        "NN -> C",
+        "BH -> H",
+        "NC -> B",
+        "NB -> B",
+        "BN -> B",
+        "BB -> N",
+        "BC -> B",
+        "CC -> N",
+        "CN -> C"
+    };
+
+
+    [Fact]
+    public void PartOne_WhenCalled_DoesNotThrowNotImplementedException()
+    {
+        Action act = () => _sut.PartOne(_sut.Input());
+
+        act.Should().NotThrow<NotImplementedException>();
+    }
+    
+    [Fact]
+    public void PartOne_WhenCalled_ReturnsCorrectTestAnswer()
+    {
+        var actual = _sut.PartOne(_testData);
+
+        actual.Should().Be("1588");
+    }
+
+
+    [Fact]
+    public void PartTwo_WhenCalled_DoesNotThrowNotImplementedException()
+    {
+        Action act = () => _sut.PartTwo(_sut.Input());
+
+        act.Should().NotThrow<NotImplementedException>();
+    }
+    
+    [Fact]
+    public void PartTwo_WhenCalled_ReturnsCorrectTestAnswer()
+    {
+        var actual = _sut.PartTwo(_testData);
+
+        actual.Should().Be("2188189693529");
+    }
+}
